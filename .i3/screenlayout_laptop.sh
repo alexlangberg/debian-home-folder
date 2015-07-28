a3bf9bbe037b4e5487c34692ab5846903371515f
Alex Langberg <mail@alexlangberg.dk>
2015-07-28 20:24:48 +0200

    Update.

--- .i3/screenlayout_laptop.sh
+++ .i3/screenlayout_laptop.sh
@@ -3,6 +3,6 @@
 xrandr --output DP1-1 --off \
 --output DP1-2-2 --off
 # turn on laptop display and turn off last monitor
-xrandr --output eDP1 --mode 1920x1080 --rate 59.9 --pos 1080x0 --output DP1-2-1 --off
+xrandr --output eDP1 --mode 1920x1080 --rate 59.9 --pos 1080x0 --primary --output DP1-2-1 --off
        
 
